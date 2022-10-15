67b2b4706a620135d66d7f318f1b3aa3b78029b8
al-uminium <aeronafic@gmail.com>
2022-10-15 20:01:03 +0800

    added functionality to summary, updated summary form

--- src/Summary.js
+++ src/Summary.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './styles/Summary.css'
 import edit from './styles/icons/edit.svg'
+import SummaryForm from './subcomponents/SummaryForm'
 
 export default class Summary extends Component {
     constructor(props) {
@@ -9,16 +10,51 @@ export default class Summary extends Component {
         this.state = {
             summary: 'Minim nostrud pariatur mollit esse quis nulla veniam nulla consectetur mollit ex in. Deserunt officia exercitation proident officia laborum. Proident ex ut mollit aliqua labore. Excepteur consequat sint cillum ex occaecat ea aliqua fugiat pariatur enim. Incididunt cupidatat amet nostrud veniam mollit tempor eu magna. Ea mollit Lorem aliquip anim aliqua ex velit esse proident qui et ad cupidatat. Reprehenderit sit ipsum voluptate labore magna officia magna ipsum sunt eiusmod.',
             editSummary: '',
+            editClicked: false,
         }
+
+        this.handleOnClick = this.handleOnClick.bind(this);
+        this.changeSummary = this.changeSummary.bind(this);
+        this.onSummaryFormSubmit = this.onSummaryFormSubmit.bind(this);
     }
 
+    handleOnClick() {
+        let clickedState = this.state.editClicked;
+        this.setState({
+            editClicked: !clickedState
+        });
+    }
+
+    changeSummary(e) {
+        this.setState({
+            editSummary: e.target.value
+        })
+    }
+
+    onSummaryFormSubmit(e) {
+        e.preventDefault()
+        this.setState({
+            summary: this.state.editSummary
+        })
+    } 
+
     render() {
-        const { summary } = this.state
+        const { summary, editClicked } = this.state
         return (
             <div className='Summary'>
                 <div className='sectionHeader'>
                     <h3>Summary</h3>
-                    <img src={edit} alt='edit'></img>
+                    <div className='btnContainer'>
+                        <img src={edit} alt='edit' className='edit' onClick={this.handleOnClick}></img>
+                        {editClicked ? 
+                            <SummaryForm 
+                                updateSummary={this.changeSummary}
+                                onFormSubmit={this.onSummaryFormSubmit}
+                            />
+                            :
+                            null
+                        }
+                    </div>
                 </div>
                 <section>
                     {summary}
--- src/subcomponents/SummaryForm.js
+++ src/subcomponents/SummaryForm.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import '../styles/SummaryForm.css'
 
-class SummaryForm extends Component {
+export default class SummaryForm extends Component {
     render() {
+        const { updateSummary, onFormSubmit } = this.props
         return (
-            <form>
-                
+            <form className='SummaryForm cvForm' onSubmit={onFormSubmit}>
+                <h4>Summary</h4>
+                <div className='inputContainer'>
+                    <label htmlFor='summary'>Enter your summary here.</label>
+                    <textarea name='summary' rows='8' col='1' required='required' onChange={updateSummary} />
+                </div>
+                <input type='submit' value='Submit' className='submitBtn'/>
             </form>
         );
     }
 }
-
-export default SummaryForm;
