bbb72b318fc974a02ded5dc9fee21be4aa6b8e8f
al-uminium <aeronafic@gmail.com>
2022-10-15 16:36:17 +0800

    added header form and styling

--- src/subcomponents/HeaderForm.js
+++ src/subcomponents/HeaderForm.js
@@ -4,8 +4,21 @@ import '../styles/HeaderForm.css'
 export default class HeaderForm extends Component {
     render() {
         return (
-            <form className='HeaderForm'>
-                Test
+            <form className='HeaderForm cvForm'>
+                <h4>Contact Information</h4>
+                <div className='inputContainer'>
+                    <label for='name'>Name</label>
+                    <input type='text' name='name' />
+                </div>
+                <div className='inputContainer'>
+                    <label for='contact'>Contact</label>
+                    <input type='text' name='contact' />
+                </div>
+                <div className='inputContainer'>
+                    <label for='email'>Email</label>
+                    <input type='text' name='email' />
+                </div>
+                <input type='submit' value='Submit' className='submitBtn'/>
             </form>
         )
     }
