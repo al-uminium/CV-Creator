372e2f3d66eb3a440d6986ced899258ca50535fc
al-uminium <aeronafic@gmail.com>
2022-10-15 17:32:28 +0800

    added skeleton summary form + css

--- src/Summary.js
+++ src/Summary.js
@@ -7,7 +7,8 @@ export default class Summary extends Component {
         super(props) 
 
         this.state = {
-            summary: 'Minim nostrud pariatur mollit esse quis nulla veniam nulla consectetur mollit ex in. Deserunt officia exercitation proident officia laborum. Proident ex ut mollit aliqua labore. Excepteur consequat sint cillum ex occaecat ea aliqua fugiat pariatur enim. Incididunt cupidatat amet nostrud veniam mollit tempor eu magna. Ea mollit Lorem aliquip anim aliqua ex velit esse proident qui et ad cupidatat. Reprehenderit sit ipsum voluptate labore magna officia magna ipsum sunt eiusmod.'
+            summary: 'Minim nostrud pariatur mollit esse quis nulla veniam nulla consectetur mollit ex in. Deserunt officia exercitation proident officia laborum. Proident ex ut mollit aliqua labore. Excepteur consequat sint cillum ex occaecat ea aliqua fugiat pariatur enim. Incididunt cupidatat amet nostrud veniam mollit tempor eu magna. Ea mollit Lorem aliquip anim aliqua ex velit esse proident qui et ad cupidatat. Reprehenderit sit ipsum voluptate labore magna officia magna ipsum sunt eiusmod.',
+            editSummary: '',
         }
     }
 
---
+++ src/subcomponents/SummaryForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SummaryForm extends Component {
+    render() {
+        return (
+            <form>
+                
+            </form>
+        );
+    }
+}
+
+export default SummaryForm;
