a18d980906f75f837c4b768bd2898e7dea26c258
al-uminium <aeronafic@gmail.com>
2022-10-25 16:15:25 +0800

    updated generated button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,25 @@ import Education from "./Education";
 
 export default class App extends Component {
 
+    generateApp() {
+        let app = document.querySelector('.App');
+        let printWindow = window.open('','','height="842px width="595px')
+        let educationcss = '<link rel="stylesheet" type="text/css" href="/src/styles/Education.css">'
+        let experiencecss = '<link rel="stylesheet" type="text/css" href="./styles/Experience.css">'
+        let headercss = '<link rel="stylesheet" type="text/css" href="./styles/Header.css">'
+        let indexcss = '<link rel="stylesheet" type="text/css" href="./styles/index.css">'
+        let instructionscss = '<link rel="stylesheet" type="text/css" href="./styles/Instructions.css">'
+        let skillscss = '<link rel="stylesheet" type="text/css" href="./styles/Skills.css">'
+        let summarycss = '<link rel="stylesheet" type="text/css" href="./styles/Summary.css">'
+        let appcss = '<link rel="stylesheet" type="text/css" href="./styles/App.css">'
+        printWindow.document.write('<!DOCTYPE html>')
+        printWindow.document.write('<html><head>'+appcss+indexcss+educationcss+experiencecss+headercss+instructionscss+skillscss+summarycss+'</head><body>')
+        printWindow.document.write(app.innerHTML)
+        printWindow.document.write("</body></html>")
+        printWindow.document.close()
+        printWindow.print()
+    }
+
     render() {
         return (
             <div className="Main">
@@ -20,7 +39,7 @@ export default class App extends Component {
                     <Experience />
                     <Education />
                 </div>
-                <button type="button" className="generate">Generate Me!</button>
+                <button type="button" className="generate" onClick={this.generateApp}>Generate Me!</button>
             </div>
         )
     }
