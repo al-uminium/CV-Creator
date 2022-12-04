b4722b5b9cc012c1594d760eb2b608cb7920418e
al-uminium <aeronafic@gmail.com>
2022-12-04 16:09:01 +0800

    changed summary components from class to hook components

--- src/Summary.js
+++ src/Summary.js
@@ -1,65 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import './styles/Summary.css'
 import edit from './styles/icons/edit.svg'
 import SummaryForm from './subcomponents/SummaryForm'
 
-export default class Summary extends Component {
-    constructor(props) {
-        super(props) 
+const Summary = () => {
+    const [summary, setSummary] = useState('Minim nostrud pariatur mollit esse quis nulla veniam nulla consectetur mollit ex in. Deserunt officia exercitation proident officia laborum. Proident ex ut mollit aliqua labore. Excepteur consequat sint cillum ex occaecat ea aliqua fugiat pariatur enim. Incididunt cupidatat amet nostrud veniam mollit tempor eu magna. Ea mollit Lorem aliquip anim aliqua ex velit esse proident qui et ad cupidatat. Reprehenderit sit ipsum voluptate labore magna officia magna ipsum sunt eiusmod.');
+    const [editSummary, setEditSummary] = useState('');
+    const [editClicked, setEditClicked] = useState(false);
 
-        this.state = {
-            summary: 'Minim nostrud pariatur mollit esse quis nulla veniam nulla consectetur mollit ex in. Deserunt officia exercitation proident officia laborum. Proident ex ut mollit aliqua labore. Excepteur consequat sint cillum ex occaecat ea aliqua fugiat pariatur enim. Incididunt cupidatat amet nostrud veniam mollit tempor eu magna. Ea mollit Lorem aliquip anim aliqua ex velit esse proident qui et ad cupidatat. Reprehenderit sit ipsum voluptate labore magna officia magna ipsum sunt eiusmod.',
-            editSummary: '',
-            editClicked: false,
-        }
-
-        this.handleOnClick = this.handleOnClick.bind(this);
-        this.changeSummary = this.changeSummary.bind(this);
-        this.onSummaryFormSubmit = this.onSummaryFormSubmit.bind(this);
-    }
-
-    handleOnClick() {
-        let clickedState = this.state.editClicked;
-        this.setState({
-            editClicked: !clickedState
-        });
-    }
-
-    changeSummary(e) {
-        this.setState({
-            editSummary: e.target.value
-        })
-    }
-
-    onSummaryFormSubmit(e) {
-        e.preventDefault()
-        this.setState({
-            summary: this.state.editSummary
-        })
-    } 
-
-    render() {
-        const { summary, editClicked } = this.state
-        return (
-            <div className='Summary'>
-                <div className='sectionHeader'>
-                    <h3>Summary</h3>
-                    <div className='btnContainer'>
-                        <img src={edit} alt='edit' className='edit' onClick={this.handleOnClick}></img>
-                        {editClicked ? 
-                            <SummaryForm 
-                                updateSummary={this.changeSummary}
-                                onFormSubmit={this.onSummaryFormSubmit}
-                            />
-                            :
-                            null
-                        }
-                    </div>
+    return (
+        <div className='Summary'>
+            <div className='sectionHeader'>
+                <h3>Summary</h3>
+                <div className='btnContainer'>
+                    <img src={edit} alt='edit' className='edit' onClick={() => setEditClicked(!editClicked)} id="ignore"></img>
+                    {editClicked ? 
+                        <SummaryForm 
+                            setSummary={setSummary}
+                            editSummary={editSummary}
+                            setEditSummary={setEditSummary}
+                            editClicked={editClicked}
+                            setEditClicked={setEditClicked}
+                        />
+                        :
+                        null
+                    }
                 </div>
-                <section>
-                    {summary}
-                </section>
             </div>
-        )
-    }
+            <section>
+                {summary}
+            </section>
+        </div>
+    )
+
 }
+
+export default Summary
--- src/subcomponents/SummaryForm.js
+++ src/subcomponents/SummaryForm.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './styles/SummaryForm.css'
 
-export default class SummaryForm extends Component {
-    render() {
-        const { updateSummary, onFormSubmit } = this.props
-        return (
-            <form className='SummaryForm cvForm' onSubmit={onFormSubmit}>
-                <h4>Summary</h4>
-                <div className='inputContainer'>
-                    <label htmlFor='summary'>Enter your summary here.</label>
-                    <textarea name='summary' rows='8' col='1' required='required' onChange={updateSummary} />
-                </div>
-                <input type='submit' value='Submit' className='submitBtn'/>
-            </form>
-        );
-    }
+const SummaryForm = ({ setSummary, setEditSummary, editSummary, editClicked, setEditClicked }) => {
+
+    return (
+        <form 
+            className='SummaryForm cvForm' 
+            onSubmit={(e) => {
+                e.preventDefault()
+                setSummary(editSummary)
+                setEditClicked(!editClicked)}
+            }
+        >
+            <h4>Summary</h4>
+            <div className='inputContainer'>
+                <label htmlFor='summary'>Enter your summary here.</label>
+                <textarea name='summary' rows='8' col='1' required='required' onChange={(e) => setEditSummary(e.target.value)} />
+            </div>
+            <input type='submit' value='Submit' className='submitBtn'/>
+        </form>
+    );
+
 }
+
+export default SummaryForm
+
+// export default class SummaryForm extends Component {
+//     render() {
+//         const { updateSummary, onFormSubmit } = this.props
+//         return (
+//             <form className='SummaryForm cvForm' onSubmit={onFormSubmit}>
+//                 <h4>Summary</h4>
+//                 <div className='inputContainer'>
+//                     <label htmlFor='summary'>Enter your summary here.</label>
+//                     <textarea name='summary' rows='8' col='1' required='required' onChange={updateSummary} />
+//                 </div>
+//                 <input type='submit' value='Submit' className='submitBtn'/>
+//             </form>
+//         );
+//     }
+// }
