594efe9caa9ac04a83d4b72a980bee00b8f33fc3
al-uminium <aeronafic@gmail.com>
2022-12-10 20:53:04 +0800

    removed commented out class components

--- src/subcomponents/SummaryForm.js
+++ src/subcomponents/SummaryForm.js
@@ -24,19 +24,3 @@ const SummaryForm = ({ setSummary, setEditSummary, editSummary, editClicked, set
 }
 
 export default SummaryForm
-
-// export default class SummaryForm extends Component {
-//     render() {
-//         const { updateSummary, onFormSubmit } = this.props
-//         return (
-//             <form className='SummaryForm cvForm' onSubmit={onFormSubmit}>
-//                 <h4>Summary</h4>
-//                 <div className='inputContainer'>
-//                     <label htmlFor='summary'>Enter your summary here.</label>
-//                     <textarea name='summary' rows='8' col='1' required='required' onChange={updateSummary} />
-//                 </div>
-//                 <input type='submit' value='Submit' className='submitBtn'/>
-//             </form>
-//         );
-//     }
-// }
