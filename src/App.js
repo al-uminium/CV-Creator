7dfab0e60ae047d39f3e3543441b90db69530326
al-uminium <aeronafic@gmail.com>
2022-12-10 20:51:31 +0800

    updated app to include print

--- src/App.js
+++ src/App.js
@@ -6,26 +6,40 @@ import Skills from "./Skills";
 import Instructions from "./Instructions";
 import Experience from "./Experience";
 import Education from "./Education";
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
 
 export default class App extends Component {
 
     generateApp() {
         let app = document.querySelector('.App');
-        let printWindow = window.open('','','height="842px width="595px')
-        let educationcss = '<link rel="stylesheet" type="text/css" href="/src/styles/Education.css">'
-        let experiencecss = '<link rel="stylesheet" type="text/css" href="./styles/Experience.css">'
-        let headercss = '<link rel="stylesheet" type="text/css" href="./styles/Header.css">'
-        let indexcss = '<link rel="stylesheet" type="text/css" href="./styles/index.css">'
-        let instructionscss = '<link rel="stylesheet" type="text/css" href="./styles/Instructions.css">'
-        let skillscss = '<link rel="stylesheet" type="text/css" href="./styles/Skills.css">'
-        let summarycss = '<link rel="stylesheet" type="text/css" href="./styles/Summary.css">'
-        let appcss = '<link rel="stylesheet" type="text/css" href="./styles/App.css">'
-        printWindow.document.write('<!DOCTYPE html>')
-        printWindow.document.write('<html><head>'+appcss+indexcss+educationcss+experiencecss+headercss+instructionscss+skillscss+summarycss+'</head><body>')
-        printWindow.document.write(app.innerHTML)
-        printWindow.document.write("</body></html>")
-        printWindow.document.close()
-        printWindow.print()
+        let appClone = app.cloneNode(true);
+        // let printWindow = window.open('','','height="842px width="595px')
+        // let educationcss = '<link rel="stylesheet" type="text/css" href="/src/styles/Education.css">'
+        // let experiencecss = '<link rel="stylesheet" type="text/css" href="./styles/Experience.css">'
+        // let headercss = '<link rel="stylesheet" type="text/css" href="./styles/Header.css">'
+        // let indexcss = '<link rel="stylesheet" type="text/css" href="./styles/index.css">'
+        // let instructionscss = '<link rel="stylesheet" type="text/css" href="./styles/Instructions.css">'
+        // let skillscss = '<link rel="stylesheet" type="text/css" href="./styles/Skills.css">'
+        // let summarycss = '<link rel="stylesheet" type="text/css" href="./styles/Summary.css">'
+        // let appcss = '<link rel="stylesheet" type="text/css" href="./styles/App.css">'
+        // printWindow.document.write('<!DOCTYPE html>')
+        // printWindow.document.write('<html><head>'+appcss+indexcss+educationcss+experiencecss+headercss+instructionscss+skillscss+summarycss+'</head><body>')
+        // printWindow.document.write(app.innerHTML)
+        // printWindow.document.write("</body></html>")
+        // printWindow.document.close()
+        // printWindow.print()
+
+        // const doc = new jsPDF();
+        // doc.html(appClone, {
+        //     callback: function(doc) {
+        //         doc.save('test.pdf')
+        //     }
+        // })
+
+        html2canvas(appClone).then(canvas => {
+            document.body.appendChild(canvas)
+        })
     }
 
     render() {
